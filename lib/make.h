a0fcbf0fc6be82358c620c0ddf69f20ac35241ef
RicannUbuntu <ricann@yeah.net>
2015-05-25 01:52:29 -0700

    change Makefile to generate .a and .so files

--- lib/make.h
+++ lib/make.h
@@ -1,13 +1,14 @@
 #compile related
 CC = echo $(notdir $<);gcc
 AS = as
-LD = echo $(notdir $@);gcc
+AR = echo $(notdir $<);ar
+LD = echo $(notdir $@);ld
 RM = rm
 MAKE = make
 MKDIR = mkdir
 
 CC_FLAGS = -c -g -O2 -Wall
-LIB_A_FLAGS = -shared
+LIB_A_FLAGS = -r
 LIB_SO_FLAGS = -shared -fPic
 LD_FLAGS =
 RM_FLAGS = -f
