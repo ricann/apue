bdb67ece2f1b415f76ce4b48e08f2e27ff524020
RicannUbuntu <ricann@yeah.net>
2015-05-28 23:50:16 -0700

    handle interupt

--- lib/src/libunp.c
+++ lib/src/libunp.c
@@ -31,10 +31,10 @@ ssize_t readn(int fd, void *ptr, size_t n)
   nleft = n;
   while(nleft > 0) {
     if( (nread = read(fd, ptr, nleft)) < 0) {
-      if(nleft == n)
-        return -1; //error, return -1
+      if (errno == EINTR)
+        nread = 0;    //and call read again
       else
-        break; //error, return amount read so far
+        return -1; //error
     } else if(nread == 0) {
       break; //EOF
     }
@@ -65,10 +65,10 @@ ssize_t writen(int fd, const void *ptr, size_t n)
   nleft = n;
   while(nleft > 0) {
     if( (nwritten = write(fd, ptr, nleft)) < 0) {
-      if(nleft == n)
-        return -1; //error, return -1
+      if (nwritten < 0 && errno == EINTR)
+        nwritten = 0;   //and call write again
       else
-        break; //error, return amount written so far
+        return -1; //error
     } else if(nwritten == 0) {
       break;
     }
