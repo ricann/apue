a0f4933008d5ee1afde1b07ecc651ce1cde625fd
RicannMac <ricann@yeah.net>
2015-06-02 23:22:43 +0800

    增加错误处理函数

--- lib/inc/libunp.h
+++ lib/inc/libunp.h
@@ -1,8 +1,35 @@
 #ifndef __LIBUNP_H__
 #define __LIBUNP_H__
 
-ssize_t readn(int fd, void *ptr, size_t n);
-ssize_t writen(int fd, const void *ptr, size_t n);
+//head files
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//macros
+#define MAXLINE   4096  //max text line length
+#define BUFFSIZE  8192  //buffer size for reads and writes
+
+
+//read and write related
+ssize_t Readn(int fd, void *ptr, size_t nbytes);
+ssize_t Writen(int fd, const void *ptr, size_t nbytes);
+
+//for error information
+void err_ret(const char *fmt, ...);
+void err_sys(const char *fmt, ...);
+void err_dump(const char *fmt, ...);
+void err_msg(const char *fmt, ...);
+void err_quit(const char *fmt, ...);
 
 #endif /* __LIBUNP_H__ */
--- lib/make.h
+++ lib/make.h
@@ -1,4 +1,4 @@
-#compile related
+#---------------compile related--------
 CC = echo $(notdir $<);gcc
 AS = as
 AR = echo $(notdir $<);ar
@@ -14,9 +14,14 @@ LD_FLAGS =
 RM_FLAGS = -f
 MKDIR_FLAGS =
 
+#----------------path------------------
 LIB_BASE = ${UNP_BASE}/lib
 LIB_INC_DIR = ${UNP_BASE}/lib/inc
 LIB_SRC_DIR = ${UNP_BASE}/lib/src
 LIB_OBJ_DIR = ${UNP_BASE}/lib/obj
 
-LIB_INC = -I${LIB_INC_DIR}
+
+#---------------variables--------------
+LIB_INCS = -I${LIB_INC_DIR}
+LIB_OBJS = ${LIB_OBJ_DIR}/librw.o \
+          ${LIB_OBJ_DIR}/liberr.o
