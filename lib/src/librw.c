88e1d628e4d791a4ccb240d873fa4f4ee9aa9731
RicannMac <ricann@yeah.net>
2015-06-05 17:03:27 +0800

    增加readline的一个慢速版本
    使用read，一次读一个字节，速度较慢

--- lib/src/librw.c
+++ lib/src/librw.c
@@ -107,3 +107,57 @@ Writen(int fd, const void *ptr, size_t nbytes)
 
   return n;
 }
+
+/******************************************************************************
+*Function: readline
+*Description： read a line from a file descriptor
+*Input: fd(file descriptor) / n(number)
+*Output: vptr(output buffer)
+*Return: read bytes
+*Date: 2015/6/5
+******************************************************************************/
+ssize_t
+readline(int fd, void *vptr, size_t maxlen)
+{
+  ssize_t n, rc;
+  char c, *ptr;
+
+  ptr = vptr;
+  for(n=1; n<maxlen; n++) {
+  again:
+    if( (rc = read(fd, &c, 1)) == 1) {
+      *ptr++ = c;
+      if(c == '\n')
+        break;          //new line is stored, like fgets()
+    } else if(rc == 0) {
+      *ptr = '\0';
+      return (n - 1);   //EOF, n-1 bytes were read
+    } else {
+      if(errno == EINTR)
+        goto again;
+      return -1;        //error, errno set by read()
+    }
+  }
+
+  *ptr = '\0';          //null terminate like fgets()
+  return n;
+}
+
+/******************************************************************************
+*Function: Readline
+*Description： read a line from a file descriptor
+*Input: fd(file descriptor) / n(number)
+*Output: vptr(output buffer)
+*Return: read bytes
+*Date: 2015/6/5
+******************************************************************************/
+ssize_t
+Readline(int fd, void *vptr, size_t maxlen)
+{
+  ssize_t n;
+
+  if( (n = readline(fd, vptr, maxlen)) < 0)
+    err_sys("readline error");
+
+  return n;
+}
