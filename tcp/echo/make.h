07b88e13759e54625c16136b9fd9358ea36026f6
RicannMac <ricann@yeah.net>
2015-06-06 23:01:34 +0800

    在tcp/echo中使用unp的make.h

--- tcp/echo/make.h
+++ tcp/echo/make.h
@@ -1,17 +1,3 @@
-#compile related
-CC = echo $(notdir $<);gcc
-AS = as
-LD = echo $(notdir $@);gcc
-RM = rm
-MAKE = make
-MKDIR = mkdir
-
-CC_FLAGS = -c -g -O2 -Wall
-LD_FLAGS =
-RM_FLAGS = -f
-MKDIR_FLAGS =
 
 #related directories
-LIB_BASE = ${UNP_BASE}/lib
-LIB_INC_BASE = ${UNP_BASE}/lib/inc
 TCP_ECHO_BASE = ${UNP_BASE}/tcp/echo
