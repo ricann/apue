7face584336f4ff60ac0d0a3189de0c628ac121f
RicannMac <ricann@yeah.net>
2015-06-06 22:58:58 +0800

    在lib中使用ump下的make.h

--- lib/make.h
+++ lib/make.h
@@ -1,21 +1,5 @@
-#---------------compile related--------
-CC = echo $(notdir $<);gcc
-AS = as
-AR = echo $(notdir $<);ar
-LD = echo $(notdir $@);ld
-RM = rm
-MAKE = make
-MKDIR = mkdir
-
-CC_FLAGS = -c -g -O2 -Wall
-LIB_A_FLAGS = -r
-LIB_SO_FLAGS = -shared -fPic
-LD_FLAGS =
-RM_FLAGS = -f
-MKDIR_FLAGS =
 
 #----------------path------------------
-LIB_BASE = ${UNP_BASE}/lib
 LIB_INC_DIR = ${UNP_BASE}/lib/inc
 LIB_SRC_DIR = ${UNP_BASE}/lib/src
 LIB_OBJ_DIR = ${UNP_BASE}/lib/obj
